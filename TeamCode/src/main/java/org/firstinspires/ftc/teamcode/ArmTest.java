33c10ffc2f8c03b0d9b39b151f77d92f6c92d11a
bluebird367 <indigobird@tutanota.com>
2023-10-05 17:04:24 -0500

    tested arm code
    managed to get numbers to show up on driver hub

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class ArmTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        DcMotor armMotor = hardwareMap.get(DcMotor.class, "front_left_motor");
+        armMotor.setTargetPosition(armMotor.getCurrentPosition());
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double max;
+
+            double angle  = gamepad1.right_stick_y * 20;
+            telemetry.addData("angle", angle);
+            telemetry.update();
+
+
+
+            armMotor.setTargetPosition((int)angle);
+        }
+    }
+}
