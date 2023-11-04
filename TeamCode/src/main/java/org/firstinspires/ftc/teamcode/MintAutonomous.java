3e22dae64cfb0362bef9d0d9fd2c584f43717055
WuffleQu3en <144185700+WuffleQu3en@users.noreply.github.com>
2023-11-04 13:55:05 -0500

    Create Autonomous mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MintAutonomous.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class MintAutonomous extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
