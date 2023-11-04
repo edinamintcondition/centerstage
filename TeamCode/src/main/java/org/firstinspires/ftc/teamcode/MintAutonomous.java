1bb769c0394c7746f18c748dac27e68a7c2e5547
Sriya <85076049+SriyaCupcakes@users.noreply.github.com>
2023-11-04 14:06:21 -0500

    add comments about plan

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MintAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MintAutonomous.java
@@ -3,6 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+// 1. Identify Prop
+// 2. Put purple pixel next to prop
+// 3. Park in the driver station
+
 @Autonomous
 public class MintAutonomous extends LinearOpMode {
 
@@ -10,4 +14,12 @@ public class MintAutonomous extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
     }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+
 }
+
+
