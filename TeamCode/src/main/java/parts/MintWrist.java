fa106592a70f495891237624a3252276f55583c9
WuffleQu3en <144185700+WuffleQu3en@users.noreply.github.com>
2023-11-07 20:55:17 -0600

    made "working" wrist

--- TeamCode/src/main/java/parts/MintWrist.java
+++ TeamCode/src/main/java/parts/MintWrist.java
@@ -1,38 +1,56 @@
 package parts;
 
-import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.FORWARD;
-
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class MintWrist {
 
     // Constants
-    String servoName = "test_servo";
+    String servoName = "wrist_servo";
+    final double FINAL_POSITION = 0.5;
+    final double START_POSITION = 0;
 
     // Variables
     Gamepad gamepad;
     Telemetry telemetry;
-    CRServo myServo;
+    Servo myServo;
 
     // Constructor
     public MintWrist(HardwareMap hardwareMap, Gamepad gamepadToUse, Telemetry aTelemetry) {
         gamepad = gamepadToUse;
 
-        myServo = hardwareMap.get(CRServo.class, servoName);
+        myServo = hardwareMap.get(Servo.class, servoName);
 
         telemetry = aTelemetry;
     }
 
     //Methods
     public void run() {
+        String pressedButton = null;
         // move servo
-        double tgtPower = -gamepad.left_stick_y;
-        myServo.setPower(tgtPower);
+        if (gamepad.dpad_up) {
+            pressedButton = "dpad up";
+            rotateClock(); // rotate clockwise;
+        } else if (gamepad.dpad_down) {
+            pressedButton = "dpad down";
+            rotateCounterClock(); // rotate counterclockwise
+//        } else {
+            //          stop(); // stops rotation
+            //        pressedButton = "nothing";
+        }
+
+        telemetry.addData(">", pressedButton + " is pressed :D");
+    }
+
+    public void rotateClock() {
+        myServo.setPosition(FINAL_POSITION); // rotate clockwise
+    }
+
+    public void rotateCounterClock() {
+        myServo.setPosition(START_POSITION); // rotate counterclockwise
     }
 
 
