85085378971defa278fe2004980e7ad59bf30f73
Avner Hegerle <115424524+Avner-Hegerle@users.noreply.github.com>
2023-11-11 08:17:29 -0600

    post merge conflict fix

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicControls.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_TO_POSITION;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class BasicControls extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        //These are the names on the driver hub
+        DcMotor leftFrontDrive = hardwareMap.get(DcMotor.class, "front_left_motor");
+        DcMotor rightFrontDrive = hardwareMap.get(DcMotor.class, "front_right_motor");
+        DcMotor leftBackDrive = hardwareMap.get(DcMotor.class, "back_left_motor");
+        DcMotor rightBackDrive = hardwareMap.get(DcMotor.class, "back_right_motor");
+        DcMotor armMotor = hardwareMap.get(DcMotor.class, "arm_motor");
+        Servo testServo = hardwareMap.get(Servo.class, "test_servo");
+
+        //Sets motor direction
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+
+
+        waitForStart();
+        while (opModeIsActive()) {
+            double max;
+
+            telemetry.addData("LS X",gamepad1.left_stick_x);
+            telemetry.update();
+
+            //These turn a joystick input into a number
+            double axial = -gamepad1.left_stick_y;
+            double lateral = gamepad1.left_stick_x;
+            double yaw = gamepad1.right_stick_x;
+            double leftFrontPower = axial - lateral + yaw;
+            double rightFrontPower = axial + lateral - yaw;
+            double leftBackPower = axial + lateral + yaw;
+            double rightBackPower = axial - lateral - yaw;
+
+            //Arm
+            /*double angle = gamepad2.right_stick_y * 45;
+            telemetry.addData("angle", angle);
+            telemetry.addData("Current Position", armMotor.getCurrentPosition());
+            telemetry.update();
+            armMotor.setTargetPosition((int) angle);
+            armMotor.setPower(1);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            /*double armPower = gamepad2.right_stick_y * 0.5;
+            armMotor.setPower(armPower);*/
+
+            //Max power of any motor, either direction
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+
+            //Sets power to 60% unless RB is pressed on game pad 1
+            double powerLimit = 0.20;
+            if (gamepad1.right_bumper)
+                powerLimit = 1.0;
+            if (max > powerLimit) {
+                leftFrontPower *= powerLimit / max;
+                rightFrontPower *= powerLimit / max;
+                leftBackPower *= powerLimit / max;
+                rightBackPower *= powerLimit / max;
+            }
+
+            //Applies the power to the motors
+            leftFrontDrive.setPower(leftFrontPower);
+            leftBackDrive.setPower(leftBackPower);
+            rightFrontDrive.setPower(rightFrontPower);
+            rightBackDrive.setPower(rightBackPower);
+
+//            double pos = testServo.getPosition();
+//            if(gamepad2.x)
+//                testServo.setPosition(pos - 0.1);
+//
+//            if(gamepad2.y)
+//                testServo.setPosition(pos + 0.1);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NavigationTest.java
@@ -0,0 +1,104 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+@Autonomous
+public class NavigationTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        AprilTagPositioning pos = new AprilTagPositioning();
+
+        WebcamName camera1 = hardwareMap.get(WebcamName.class, "Webcam 1");
+        VisionPortal myVisionPort = VisionPortal.easyCreateWithDefaults(camera1, pos.myAprilTagProc);
+
+        DcMotor leftFrontDrive = hardwareMap.get(DcMotor.class, "front_left_motor");
+        DcMotor rightFrontDrive = hardwareMap.get(DcMotor.class, "front_right_motor");
+        DcMotor leftBackDrive = hardwareMap.get(DcMotor.class, "back_left_motor");
+        DcMotor rightBackDrive = hardwareMap.get(DcMotor.class, "back_right_motor");
+
+        //Sets motor direction
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            Position currentPos = pos.getPosition();
+            if (currentPos == null)
+                continue;
+
+            double rx = currentPos.x;
+            double ry = currentPos.y;
+            double a = currentPos.a;
+            double dx = currentPos.dx;
+            double dy = currentPos.dy;
+
+            double goalY = 120;
+            double goalX = 98;
+            double goalYaw = 0;
+
+            double ty = goalY - ry;
+            double tx = goalX - rx;
+
+            double axial = (tx * dx) + (ty * dy);
+            double lateral = (tx * dy) - (ty * dx);
+            double yaw = a - goalYaw;
+            yaw = 0;
+
+            if (yaw < -180) {
+                yaw = yaw + 360;
+            }
+
+            if (yaw > 180) {
+                yaw = yaw - 360;
+            }
+
+            //These turn a joystick input into a number
+            double leftFrontPower = axial - lateral + yaw;
+            double rightFrontPower = axial + lateral - yaw;
+            double leftBackPower = axial + lateral + yaw;
+            double rightBackPower = axial - lateral - yaw;
+
+            //Max power of any motor, either direction
+            double max;
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+
+            double powerLimit = 0.25;
+            if (max > powerLimit) {
+                leftFrontPower *= powerLimit / max;
+                rightFrontPower *= powerLimit / max;
+                leftBackPower *= powerLimit / max;
+                rightBackPower *= powerLimit / max;
+            }
+
+            //Applies the power to the motors
+            leftFrontDrive.setPower(leftFrontPower);
+            leftBackDrive.setPower(leftBackPower);
+            rightFrontDrive.setPower(rightFrontPower);
+            rightBackDrive.setPower(rightBackPower);
+
+            telemetry.addData("rx", rx);
+            telemetry.addData("ry", ry);
+            telemetry.addData("lateral", lateral);
+            telemetry.addData("axial", axial);
+            telemetry.addData("yaw", yaw);
+            telemetry.update();
+        }
+    }
+}
