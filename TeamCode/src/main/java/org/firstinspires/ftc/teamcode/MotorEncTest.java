372d77fdba200156b831af14600801ca898782eb
Avner Hegerle <115424524+Avner-Hegerle@users.noreply.github.com>
2023-11-14 19:36:24 -0600

    encoder driving test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorEncTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorEncTest.java
@@ -5,9 +5,19 @@ import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENC
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.hardware.bosch.BNO055IMUNew;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @Autonomous
 public class MotorEncTest extends LinearOpMode {
@@ -23,42 +33,130 @@ public class MotorEncTest extends LinearOpMode {
         rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        double ppi= 537.0*5/60.875;
+        DcMotor[] motors = new DcMotor[]{
+                leftFront,
+                leftBack,
+                rightFront,
+                rightBack
+        };
 
-        int targetDist = 30;
+        DcMotor[] frontMotors = new DcMotor[]{
+                leftFront,
+                rightBack
+        };
+
+        DcMotor[] backMotors = new DcMotor[]{
+                leftBack,
+                rightFront
+        };
 
         waitForStart();
 
+        for (DcMotor m : motors) {
+            m.setMode(STOP_AND_RESET_ENCODER);
+        }
 
-        leftFront.setMode(STOP_AND_RESET_ENCODER);
-        rightFront.setMode(STOP_AND_RESET_ENCODER);
-        leftBack.setMode(STOP_AND_RESET_ENCODER);
-        rightBack.setMode(STOP_AND_RESET_ENCODER);
+        strafeDistance(3.75, frontMotors, backMotors);
+        sleep(200);
+        driveDistance(72, motors);
+        sleep(200);
+        strafeDistance(24, frontMotors, backMotors);
+        sleep(200);
+        driveDistance(10, motors);
+        sleep(200);
+        driveDistance(-10, motors);
+        sleep(200);
+        rotateAmount(180, motors);
+    }
 
-        telemetry.addData("c pos", "%d %d %d %d", leftFront.getCurrentPosition(), leftBack.getCurrentPosition(), rightFront.getCurrentPosition(), rightBack.getCurrentPosition());
-        telemetry.update();
+    private void driveDistance(double targetDist, DcMotor[] motors) {
+        double fastLimit = 10;
+        double ppi = 537.0 * 5 / 60.875;
 
-        int targetPos = (int)(targetDist*ppi);
+        int targetPos = (int) (targetDist * ppi);
+        double power;
 
-        leftFront.setTargetPosition(targetPos);
-        rightFront.setTargetPosition(targetPos);
-        leftBack.setTargetPosition(targetPos);
-        rightBack.setTargetPosition(targetPos);
+        if (targetDist > fastLimit) {
+            power = 1;
+        } else {
+            power = .25;
+        }
 
-        leftFront.setPower(.3);
-        rightFront.setPower(.3);
-        leftBack.setPower(.3);
-        rightBack.setPower(.3);
-
-        leftFront.setMode(RUN_TO_POSITION);
-        rightFront.setMode(RUN_TO_POSITION);
-        leftBack.setMode(RUN_TO_POSITION);
-        rightBack.setMode(RUN_TO_POSITION);
+        for (DcMotor m : motors) {
+            m.setTargetPosition(targetPos);
+            m.setPower(power);
+            m.setMode(RUN_TO_POSITION);
+        }
 
         while (opModeIsActive()) {
-            if (!leftFront.isBusy()) {
+            if (!areBusy(motors)) {
                 break;
             }
         }
+    }
+
+    private void rotateAmount(double targetAngle, DcMotor[] motors) {
+        double ppd = 537.0 / 63.15;
+
+        int targetPos = (int) (targetAngle * ppd);
+        double power = 0.5;
+
+        for (DcMotor m : motors) {
+            if (m.getDirection() == DcMotorSimple.Direction.FORWARD) {
+                m.setTargetPosition(-targetPos);
+            } else {
+                m.setTargetPosition(targetPos);
+            }
+
+            m.setPower(power);
+            m.setMode(RUN_TO_POSITION);
+        }
+
+        while (opModeIsActive()) {
+            if (!areBusy(motors)) {
+                break;
+            }
+        }
+    }
+
+    private void strafeDistance(double targetDist, DcMotor[] frontMotors, DcMotor[] backMotors) {
+        double fastLimit = 10;
+        double ppi = 50.09;
+
+        int targetPos = (int) (targetDist * ppi);
+        double power;
+
+        if (targetDist > fastLimit) {
+            power = 1;
+        } else {
+            power = .3;
+        }
+
+        for (DcMotor m : frontMotors) {
+            m.setTargetPosition(targetPos);
+            m.setPower(power);
+            m.setMode(RUN_TO_POSITION);
+        }
+
+        for (DcMotor m : backMotors) {
+            m.setTargetPosition(-targetPos);
+            m.setPower(power);
+            m.setMode(RUN_TO_POSITION);
+        }
+
+        while (opModeIsActive()) {
+            if (!areBusy(backMotors) && !areBusy(frontMotors)) {
+                break;
+            }
+        }
+    }
+
+    private boolean areBusy(DcMotor[] motors) {
+        for (DcMotor m : motors) {
+            if (m.isBusy()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
