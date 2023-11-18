58000bdf2e1905b75221e178b0da3b3544ea71fb
WuffleQu3en <144185700+WuffleQu3en@users.noreply.github.com>
2023-11-18 09:33:19 -0600

    Made arm too strong

--- TeamCode/src/main/java/parts/MintArm.java
+++ TeamCode/src/main/java/parts/MintArm.java
@@ -47,6 +47,19 @@ public class MintArm {
         armMotor.setPower(tgtPower);
 
         telemetry.addData(armMotor.getDeviceName(), tgtPower);
+        /* This code *literally* breaks the robot but would work with a stronger wrist servo
+        if (isStrongArm || gamepad.right_trigger > 0.8) {
+            armMotor.setPower(1.00);
+            isStrongArm = true;
+            telemetry.addData(">", "STRONGARM HAHAHA");
+
+            if (gamepad.left_trigger > 0.8) {
+                armMotor.setPower(0.00);
+                isStrongArm = false;
+                telemetry.addData(">", "Back to normal!");
+            }
+        } else {
+         */
     }
 
 }
