9b040ba78b1e1bffc6b6c8dcc3df0b47864eb3db
WuffleQu3en <144185700+WuffleQu3en@users.noreply.github.com>
2023-11-22 08:29:56 -0600

    disabled unused classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicControls.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_TO_POSITION;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,6 +11,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import parts.MintWheels;
 
 @TeleOp
+@Disabled
 public class BasicControls extends LinearOpMode {
     @Override
     public void runOpMode() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.bosch.BNO055IMUNew;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.IMU;
 
@@ -13,6 +14,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @Autonomous
+@Disabled
 public class IMUTest extends LinearOpMode {
 
     BNO055IMUNew IMU;
