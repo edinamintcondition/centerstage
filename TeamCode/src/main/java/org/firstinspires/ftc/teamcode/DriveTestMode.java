33fe80696c5d0c2b7c5392d58d3d8c9c7168905f
Avner Hegerle <115424524+Avner-Hegerle@users.noreply.github.com>
2024-01-10 21:30:40 -0600

    Added future development ideas

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTestMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTestMode.java
@@ -7,6 +7,13 @@ import parts.MintDrive;
 
 @Autonomous
 public class DriveTestMode extends LinearOpMode {
+    /*Set ERR_MULT and DRAG_MULT = 0, and calibrate.
+    Grab pos at end of acceleration, and compare to degreeStopAccel
+    ratio used to change ACCEL_MULT
+    once start cruising, start accelerometer (if you hit 0 then stop)
+    also get stats on torquefrac used in cruise mode
+    Use LinearFuncFitter to find DRAG_MULT for cruise
+    do something similar for deceleration*/
     @Override
     public void runOpMode() {
         MintDrive md = new MintDrive(hardwareMap, telemetry);
